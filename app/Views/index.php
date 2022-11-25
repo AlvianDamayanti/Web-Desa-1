3c8c0fa8c10dacf7a2edeb20946894ba65c9c324
rizailhamsyah <80843743+rizailhamsyah@users.noreply.github.com>
2022-11-25 13:01:35 +0700

    Update

---
+++ app/Views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <Link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-grid.min.css');?>">
+    <title>Home</title>
+</head>
+<body>
+    <script src="<?= base_url('assets/js/bootstrap-grid.min.js');?>"></script>
+</body>
+</html>
